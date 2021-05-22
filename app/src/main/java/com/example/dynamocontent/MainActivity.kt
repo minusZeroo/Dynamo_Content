d25d0a628c325e812adfe7bc5e21a6377c340765
minuszero <pcijideani@gmail.com>
2021-05-22 20:36:13 +0200

    some mods made

--- app/src/main/java/com/example/dynamocontent/MainActivity.kt
+++ app/src/main/java/com/example/dynamocontent/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.dynamocontent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dynamocontent.ui.theme.DynamoContentTheme
 
 //Using a list structure to store a list of names
-val namesList: List<String> = listOf("Paul", "James", "Arinze", "Thandi", "Georgia")
+val namesList: ArrayList<String> = arrayListOf("Paul", "James", "Arinze", "Thandi", "Georgia")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,16 +34,23 @@ class MainActivity : ComponentActivity() {
 //then we want to print a text
 @Composable
 fun GreetingList(names: List<String>){
-    Column() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally) {
         for (name in names){
             Greeting(name = name)
+        }
+        Button(onClick = { namesList.add("New name") }) {
+            Text(text = "Add new name")
+            
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+    style = MaterialTheme.typography.h4)
 }
 
 @Preview(showBackground = true)
