070bb7765224636d693a5a882d7aabecdf7761ef
minuszero <pcijideani@gmail.com>
2021-05-22 21:39:58 +0200

    State hoisting

--- app/src/main/java/com/example/dynamocontent/MainActivity.kt
+++ app/src/main/java/com/example/dynamocontent/MainActivity.kt
@@ -7,15 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dynamocontent.ui.theme.DynamoContentTheme
 
@@ -36,20 +35,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(){
     val greetingListState = remember { mutableStateListOf<String>("Paul", "Amanda") }
+
+    val newNameStateContent = remember { mutableStateOf(" ") }
+
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally)
     {
-        GreetingList(greetingListState) { greetingListState.add("Jideani") }
+        GreetingList(greetingListState,
+            { greetingListState.add(newNameStateContent.value) },
+            newNameStateContent.value,
+            { newName -> newNameStateContent.value = newName})
     }
 }
 
 
 @Composable
-fun GreetingList(nameList: List<String>, buttonClick: () -> Unit){
+fun GreetingList(nameList: List<String>, buttonClick: () -> Unit, textFieldValue: String, textFieldUpdate: (newName: String) -> Unit){
     for (name in nameList){
         Greeting(name = name)
     }
+
+
+    TextField(value = textFieldValue, onValueChange = textFieldUpdate)
+
+
     Button(onClick = buttonClick) {
         Text(text = "Add new name")
 
