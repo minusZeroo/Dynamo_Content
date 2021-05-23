4f61ec68c3297d263732e19c871fde0c5f7abf57
minuszero <pcijideani@gmail.com>
2021-05-23 14:03:01 +0200

    Using a viewModel

--- app/src/main/java/com/example/dynamocontent/MainActivity.kt
+++ app/src/main/java/com/example/dynamocontent/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,43 +34,42 @@ class MainActivity : ComponentActivity() {
 //using the advanced for loop, which should be easy to understand
 //then we want to print a text
 @Composable
-fun MainScreen(){
-    val greetingListState = remember { mutableStateListOf<String>("Paul", "Amanda") }
+fun MainScreen(viewModel: MainViewModel = MainViewModel()) {
 
-    val newNameStateContent = remember { mutableStateOf(" ") }
+    val newNameStateContent = viewModel.textFieldState.observeAsState("")
 
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally)
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
     {
-        GreetingList(greetingListState,
-            { greetingListState.add(newNameStateContent.value) },
-            newNameStateContent.value,
-            { newName -> newNameStateContent.value = newName}) //new name the user entered
+        GreetingMessage(
+            newNameStateContent.value
+        ) { newName -> viewModel.onTextChanged(newName) } //new name the user entered
     }
 }
 
 
 @Composable
-fun GreetingList(nameList: List<String>, buttonClick: () -> Unit, textFieldValue: String, textFieldUpdate: (newName: String) -> Unit){
-    for (name in nameList){
-        Greeting(name = name)
-    }
-
-
+fun GreetingMessage(
+    textFieldValue: String,
+    textFieldUpdate: (newName: String) -> Unit
+) {
     TextField(value = textFieldValue, onValueChange = textFieldUpdate)
 
-
-    Button(onClick = buttonClick) {
-        Text(text = "Add new name")
+    Button(onClick = { }) {
+        Text(textFieldValue)
 
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!",
-    style = MaterialTheme.typography.h4)
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h4
+    )
 }
 
 @Preview(showBackground = true)
